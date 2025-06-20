21557bd5dbfac32c8622f958cded779041b9c984
Layoxd <lioneelrr.mexico@gmail.com>
2025-06-19 22:56:40 -0600

    Update logger.go

--- whatsapiGo/backend/utils/logger.go
+++ whatsapiGo/backend/utils/logger.go
@@ -1,1 +1,17 @@
-// Archivo base: logger.go
+package utils
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// SetupLogger - Configurar logger con zap
+func SetupLogger() *zap.Logger {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.StacktraceKey = ""
+	
+	logger, _ := config.Build()
+	return logger
+}// Archivo base: logger.go
